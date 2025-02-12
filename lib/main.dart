746a7031aef8fd1ae7d0694e718e1b826d45c578
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-02-12 17:28:29 +0100

    Added the riverpod provider to the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:favorite_places/screens/places.dart';
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:favorite_places/screens/places.dart';
 
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
@@ -27,7 +29,9 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-    const MyApp(),
+    const ProviderScope(
+      child: MyApp(),
+    ),
   );
 }
 

5cd6e76cb2e5e430a89212becc61a02776e09c0c
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-02-13 21:31:20 +0100

    Started the implementation of the image picking process

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:image_picker/image_picker.dart';
+
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
 
@@ -10,7 +12,10 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
-  void _takePicture() {}
+  void _takePicture() {
+    final imagePicker = ImagePicker();
+    imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+  }
 
   @override
   Widget build(BuildContext context) {

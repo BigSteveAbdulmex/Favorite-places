2f03aafa8a68715f903482c8ce57bcdc9cd5dad1
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-02-13 19:33:06 +0100

    Added the onTap parameter to link to the places details screen

--- lib/widgets/places_widget.dart
+++ lib/widgets/places_widget.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/screens/places_detail.dart';
 import 'package:flutter/material.dart';
 
 import 'package:favorite_places/models/place.dart';
@@ -29,6 +30,15 @@ class PlacesWidget extends StatelessWidget {
                 color: Theme.of(context).colorScheme.onSurface,
               ),
         ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => PlacesDetailScreen(
+                place: places[index],
+              ),
+            ),
+          );
+        },
       ),
     );
   }

94e917b685355ba8d7b2dcb210b2370c941c2d72
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-02-14 20:20:07 +0100

    Saved the image to memory

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:favorite_places/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 
@@ -17,14 +19,18 @@ class AddPlacesScreen extends ConsumerStatefulWidget {
 class _AddPlacesScreenState extends ConsumerState<AddPlacesScreen> {
   final _titleController = TextEditingController();
 
+  File? _selectedImage;
+
   void _savePlace() {
     final enteredTitle = _titleController.text;
 
-    if (enteredTitle.isEmpty) {
+    if (enteredTitle.isEmpty || _selectedImage == null) {
       return;
     }
 
-    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle);
+    ref
+        .read(userPlacesProvider.notifier)
+        .addPlace(enteredTitle, _selectedImage!);
 
     Navigator.of(context).pop();
   }
@@ -55,7 +61,11 @@ class _AddPlacesScreenState extends ConsumerState<AddPlacesScreen> {
               ),
             ),
             const SizedBox(height: 10),
-            ImageInput(),
+            ImageInput(
+              onPickImage: (image) {
+                _selectedImage = image;
+              },
+            ),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
